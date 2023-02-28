fbf23e9217059c88b215f20bb5ecc90d4eafc3fe
Gerd Faedtke <gerd.faedtke@icloud.com>
2023-02-28 09:57:50 +0100

    First Commit

--- XcodeTest-Git/ContentView.swift
+++ XcodeTest-Git/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            Button("Show Modal") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                ModalView()
+            }
+            .navigationBarTitle("Xcode and Git")
         }
-        .padding()
     }
 }
 
