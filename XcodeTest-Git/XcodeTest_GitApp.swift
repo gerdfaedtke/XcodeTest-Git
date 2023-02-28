5459bf41104bccdf20d8cf05eae43a83695e369d
Gerd Faedtke <gerd.faedtke@icloud.com>
2023-02-28 09:39:42 +0100

    Initial Commit

---
+++ XcodeTest-Git/XcodeTest_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTest_GitApp.swift
+//  XcodeTest-Git
+//
+//  Created by Gerd Faedtke on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTest_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
